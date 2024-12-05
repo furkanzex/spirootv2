09c4310d7013c4062cfd0223d22e448381c4182d
furkanzek <furkanzekiri101@gmail.com>
2024-12-05 05:00:20 +0300

    upd

--- lib/fortune/coffee/coffee_fortune_result_screen.dart
+++ lib/fortune/coffee/coffee_fortune_result_screen.dart
@@ -8,6 +8,7 @@ import 'package:scaffold_gradient_background/scaffold_gradient_background.dart';
 import 'package:spirootv2/core/constant/my_color.dart';
 import 'package:spirootv2/core/constant/my_size.dart';
 import 'package:spirootv2/core/constant/my_style.dart';
+import 'package:spirootv2/core/helper/device_helper.dart';
 import 'package:spirootv2/profile/user_controller.dart';
 
 class CoffeeFortuneResultScreen extends StatefulWidget {
@@ -30,18 +31,29 @@ class _CoffeeFortuneResultScreenState extends State<CoffeeFortuneResultScreen> {
   final TextEditingController _topic1Controller = TextEditingController();
   final TextEditingController _topic2Controller = TextEditingController();
   bool _isForSelf = true;
+  bool _hasProfile = false;
 
   @override
   void initState() {
     super.initState();
     _topic1Controller.text = tr('profile.interests.money');
     _topic2Controller.text = tr('profile.interests.career');
-    _loadUserData();
+    _checkProfile();
+  }
+
+  void _checkProfile() {
+    final userController = Get.find<UserController>();
+    setState(() {
+      _hasProfile = userController.nameController.text.isNotEmpty;
+      if (_hasProfile) {
+        _loadUserData();
+      }
+    });
   }
 
   void _loadUserData() {
     final userController = Get.find<UserController>();
-    if (_isForSelf) {
+    if (_isForSelf && _hasProfile) {
       _nameController.text = userController.nameController.text;
       _birthDateController.text = DateFormat('d MMM yyyy', 'tr_TR')
           .format(userController.selectedBirthDateTime.value);
@@ -321,196 +333,201 @@ class _CoffeeFortuneResultScreenState extends State<CoffeeFortuneResultScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ScaffoldGradientBackground(
-      gradient: LinearGradient(
-        begin: Alignment.bottomLeft,
-        end: Alignment.topRight,
-        colors: [
-          MyColor.darkBackgroundColor,
-          MyColor.primaryColor,
-        ],
-      ),
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Text(
-          'Kahve Falı',
-          style: MyStyle.s1.copyWith(
-            color: MyColor.white,
-            fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () => DeviceHelper.hideKeyboard(),
+      child: ScaffoldGradientBackground(
+        gradient: LinearGradient(
+          begin: Alignment.bottomLeft,
+          end: Alignment.topRight,
+          colors: [
+            MyColor.darkBackgroundColor,
+            MyColor.primaryColor,
+          ],
+        ),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            'Kahve Falı',
+            style: MyStyle.s1.copyWith(
+              color: MyColor.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios, color: MyColor.white),
+            onPressed: () => Navigator.pop(context),
           ),
         ),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios, color: MyColor.white),
-          onPressed: () => Navigator.pop(context),
-        ),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(MySize.defaultPadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Merak ettiğin konular?',
-              style: MyStyle.s1.copyWith(
-                color: MyColor.white,
-                fontWeight: FontWeight.bold,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.all(MySize.defaultPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Merak ettiğin konular?',
+                style: MyStyle.s1.copyWith(
+                  color: MyColor.white,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: MySize.defaultPadding),
-            Container(
-              padding: EdgeInsets.all(MySize.defaultPadding),
-              decoration: BoxDecoration(
-                color: MyColor.white.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(MySize.halfRadius),
-              ),
-              child: Column(
-                children: [
-                  _buildTopicItem('1. Konu', _topic1Controller),
-                  SizedBox(height: MySize.halfPadding),
-                  _buildTopicItem('2. Konu', _topic2Controller),
-                ],
-              ),
-            ),
-            SizedBox(height: MySize.defaultPadding),
-            Text(
-              'Fotoğraflar',
-              style: MyStyle.s1.copyWith(
-                color: MyColor.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: MySize.defaultPadding),
-            GridView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                crossAxisSpacing: MySize.halfPadding,
-                mainAxisSpacing: MySize.halfPadding,
-              ),
-              itemCount: widget.images.length,
-              itemBuilder: (context, index) {
-                return ClipRRect(
+              SizedBox(height: MySize.defaultPadding),
+              Container(
+                padding: EdgeInsets.all(MySize.defaultPadding),
+                decoration: BoxDecoration(
+                  color: MyColor.white.withOpacity(0.1),
                   borderRadius: BorderRadius.circular(MySize.halfRadius),
-                  child: Image.file(
-                    widget.images[index],
-                    fit: BoxFit.cover,
-                  ),
-                );
-              },
-            ),
-            SizedBox(height: MySize.defaultPadding),
-            Text(
-              'Kimin İçin?',
-              style: MyStyle.s1.copyWith(
-                color: MyColor.white,
-                fontWeight: FontWeight.bold,
+                ),
+                child: Column(
+                  children: [
+                    _buildTopicItem('1. Konu', _topic1Controller),
+                    SizedBox(height: MySize.halfPadding),
+                    _buildTopicItem('2. Konu', _topic2Controller),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: MySize.defaultPadding),
-            Row(
-              children: [
-                _buildSelectionButton(
-                  title: 'Kendim İçin',
-                  icon: '👍',
-                  isSelected: _isForSelf,
-                  onTap: () {
-                    setState(() {
-                      _isForSelf = true;
-                      _loadUserData();
-                    });
-                  },
+              SizedBox(height: MySize.defaultPadding),
+              Text(
+                'Fotoğraflar',
+                style: MyStyle.s1.copyWith(
+                  color: MyColor.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                SizedBox(width: MySize.defaultPadding),
-                _buildSelectionButton(
-                  title: 'Başkası İçin',
-                  icon: '👉',
-                  isSelected: !_isForSelf,
-                  onTap: () {
-                    setState(() {
-                      _isForSelf = false;
-                      _nameController.clear();
-                      _birthDateController.clear();
-                      _relationshipController.clear();
-                    });
-                  },
+              ),
+              SizedBox(height: MySize.defaultPadding),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: MySize.halfPadding,
+                  mainAxisSpacing: MySize.halfPadding,
                 ),
-              ],
-            ),
-            SizedBox(height: MySize.defaultPadding),
-            _buildTextField(
-              controller: _nameController,
-              label: 'İsim',
-              icon: Icons.person,
-            ),
-            SizedBox(height: MySize.defaultPadding),
-            _buildTextField(
-              controller: _birthDateController,
-              label: 'Doğum Günü',
-              icon: Icons.calendar_today,
-              readOnly: true,
-              onTap: () async {
-                if (!_isForSelf) {
-                  final date = await showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(1900),
-                    lastDate: DateTime.now(),
-                  );
-                  if (date != null) {
-                    _birthDateController.text =
-                        DateFormat('d MMM yyyy', 'tr_TR').format(date);
-                  }
-                }
-              },
-            ),
-            SizedBox(height: MySize.defaultPadding),
-            _buildTextField(
-              controller: _relationshipController,
-              label: 'İlişki Durumu',
-              icon: Icons.favorite,
-              readOnly: true,
-              onTap: () {
-                if (!_isForSelf) {
-                  _showRelationshipPicker();
-                }
-              },
-            ),
-            SizedBox(height: MySize.defaultPadding * 2),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: MyColor.primaryColor,
-                  padding:
-                      EdgeInsets.symmetric(vertical: MySize.defaultPadding),
-                  shape: RoundedRectangleBorder(
+                itemCount: widget.images.length,
+                itemBuilder: (context, index) {
+                  return ClipRRect(
                     borderRadius: BorderRadius.circular(MySize.halfRadius),
-                  ),
-                ),
-                onPressed: () {
-                  if (_topic1Controller.text.isEmpty ||
-                      _topic2Controller.text.isEmpty) {
-                    Get.snackbar(
-                      'Hata',
-                      'Lütfen her iki konuyu da seçin',
-                      backgroundColor: MyColor.errorColor,
-                      colorText: MyColor.white,
-                    );
-                    return;
-                  }
-                  // TODO: Fal gönderme işlemi
+                    child: Image.file(
+                      widget.images[index],
+                      fit: BoxFit.cover,
+                    ),
+                  );
                 },
-                child: Text(
-                  'Gönder',
-                  style: MyStyle.s1.copyWith(
-                    color: MyColor.white,
-                    fontWeight: FontWeight.bold,
+              ),
+              SizedBox(height: MySize.defaultPadding),
+              Text(
+                'Kimin İçin?',
+                style: MyStyle.s1.copyWith(
+                  color: MyColor.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: MySize.defaultPadding),
+              if (_hasProfile)
+                Row(
+                  children: [
+                    _buildSelectionButton(
+                      title: 'Kendim İçin',
+                      icon: '👍',
+                      isSelected: _isForSelf,
+                      onTap: () {
+                        setState(() {
+                          _isForSelf = true;
+                          _loadUserData();
+                        });
+                      },
+                    ),
+                    SizedBox(width: MySize.defaultPadding),
+                    _buildSelectionButton(
+                      title: 'Başkası İçin',
+                      icon: '👉',
+                      isSelected: !_isForSelf,
+                      onTap: () {
+                        setState(() {
+                          _isForSelf = false;
+                          _nameController.clear();
+                          _birthDateController.clear();
+                          _relationshipController.clear();
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              SizedBox(height: MySize.defaultPadding),
+              _buildTextField(
+                readOnly: _isForSelf,
+                controller: _nameController,
+                label: 'İsim',
+                icon: Icons.person,
+              ),
+              SizedBox(height: MySize.defaultPadding),
+              _buildTextField(
+                controller: _birthDateController,
+                label: 'Doğum Günü',
+                icon: Icons.calendar_today,
+                readOnly: true,
+                onTap: () async {
+                  if (!_isForSelf) {
+                    final date = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1900),
+                      lastDate: DateTime.now(),
+                    );
+                    if (date != null) {
+                      _birthDateController.text =
+                          DateFormat('d MMM yyyy', 'tr_TR').format(date);
+                    }
+                  }
+                },
+              ),
+              SizedBox(height: MySize.defaultPadding),
+              _buildTextField(
+                controller: _relationshipController,
+                label: 'İlişki Durumu',
+                icon: Icons.favorite,
+                readOnly: true,
+                onTap: () {
+                  if (!_isForSelf) {
+                    _showRelationshipPicker();
+                  }
+                },
+              ),
+              SizedBox(height: MySize.defaultPadding * 2),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: MyColor.primaryColor,
+                    padding:
+                        EdgeInsets.symmetric(vertical: MySize.defaultPadding),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(MySize.halfRadius),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_topic1Controller.text.isEmpty ||
+                        _topic2Controller.text.isEmpty) {
+                      Get.snackbar(
+                        'Hata',
+                        'Lütfen her iki konuyu da seçin',
+                        backgroundColor: MyColor.errorColor,
+                        colorText: MyColor.white,
+                      );
+                      return;
+                    }
+                    // TODO: Fal gönderme işlemi
+                  },
+                  child: Text(
+                    'Gönder',
+                    style: MyStyle.s1.copyWith(
+                      color: MyColor.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
