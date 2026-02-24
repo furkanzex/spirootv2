36a214fdf8ccdbca931d2d625cee623645d2f280
Furkan Zekiri <furkanzekiri1@gmail.com>
2026-02-24 07:46:13 +0300

    Update social_screen.dart

--- lib/social/screens/social_screen.dart
+++ lib/social/screens/social_screen.dart
@@ -40,7 +40,7 @@ class _SocialScreenState extends State<SocialScreen> {
   final _eventsScrollController = ScrollController();
   final FocusNode _locationFocusNode = FocusNode();
   final _placesApi =
-      FlutterGooglePlacesSdk("AIzaSyDri3yUianYuZw3PfZlruuFLg196-UhXE8");
+      FlutterGooglePlacesSdk("placesAPI");
   Timer? _debounce;
   String _searchQuery = '';
 
